00b8fe3b84ba76fb18f36cf9ae8eef6b866968f9
aresDanvila <aresdan678@gmail.com>
2024-12-19 15:44:59 +0100

    Añadimos el ultimo

--- Proyecto Prueba/src/Principal.java
+++ Proyecto Prueba/src/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Holi wankenobi");
 		System.out.println("Agur yogur!");
+		System.out.println("Adios de verdad");
 	}
 
 }
